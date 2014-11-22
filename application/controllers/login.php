aafd70556cc82f54309579564fec1f2e3f3c14dd
francisalfanta <francisalfanta@gmail.com>
2014-11-22 08:38:38 +0400

    Added login_form.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,6 +4,7 @@ class Login extends CI_Controller {
 	function index() {
 		$this->load->helper('url');
 		$this->load->view('layout/header');
+		$this->load->view('login_form');
 		$this->load->view('layout/footer');
 	}
 }
